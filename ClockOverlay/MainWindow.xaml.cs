71d0a75936ad11dce109649f837fdb9d5ca6ad6c
yousa <yousa@THINKBOOK-15>
2022-08-07 16:47:48 +0900

    プロジェクト ファイルを追加します。

---
+++ ClockOverlay/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ClockOverlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseLeftButtonDown += (Sender, e) => { this.DragMove(); };
+            this.MouseRightButtonUp += (Sender, e) => { Close(); };
+            var timer = new DispatcherTimer(DispatcherPriority.SystemIdle)
+            {
+                Interval = TimeSpan.FromSeconds(1),
+            };
+            timer.Tick += (Sender, e) =>
+            {
+                clock.Text = DateTime.Now.ToString("HH:mm");
+            };
+            timer.Start();
+        }
+    }
+}
